7a336518c815d0446b0e82607246053cc2700be4
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-07 21:47:42 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +21,7 @@ public class Main extends Application {
 			ScrollPane scrollPane = loader.load();
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -31,4 +34,7 @@ public class Main extends Application {
 		launch(args);
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -35,6 +44,7 @@ public class MainViewController implements Initializable{
 	public void onMenuItemAboutAction() {
 		
 		System.out.println("onMenuItemAboutAction()");
+		loadView("/gui/About.fxml");
 		
 	}
 	
@@ -42,6 +52,32 @@ public class MainViewController implements Initializable{
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		
+		
+	}
+	
+	@FXML
+	private synchronized void loadView(String absoluteName) {
+		
+		
+		try {
+			FXMLLoader  loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene= Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+			
+			
+		} catch (IOException e) {
+			
+			Alerts.showAlert("IO Exception", "Erro load view", e.getMessage(),AlertType.ERROR);
+		}
 		
 	}
 
