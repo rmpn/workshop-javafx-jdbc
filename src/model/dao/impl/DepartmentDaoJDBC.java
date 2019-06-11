cd47d0564d44c680fb3ac8c641145e3699b585eb
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-11 20:50:24 -0300

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -114,7 +115,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 		} catch (SQLException e) {
 
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 
 			DB.closeResultSet(rs);
